97b20c70539906d67e70f6ae07dcfa36bf4adc5b
vickykumar1001 <vicky9060993097@gmail.com>
2024-11-17 03:42:57 +0530

    Updated Header - passing necessary props to Dropdown

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './header.css';
 import Dropdown from '../Dropdown';
 
-const Header = () => (
+const Header = ({ grouping, setGrouping, ordering, setOrdering }) => (
     <header className="header-container">
-        <Dropdown />
+        <Dropdown grouping={grouping} setGrouping={setGrouping} ordering={ordering} setOrdering={setOrdering} />
     </header>
 );
 
