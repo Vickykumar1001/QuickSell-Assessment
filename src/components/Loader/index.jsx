715f79c57eb25c2afc2de6d419ed43811b269b28
vickykumar1001 <vicky9060993097@gmail.com>
2024-11-17 03:37:42 +0530

    Updated Loader

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -4,6 +4,7 @@ import './loader.css';
 function Loader({ fullscreen }) {
     return (
         <div className={`loader-wrapper ${fullscreen ? "fullscreen" : ""}`}>
+            <div className="spinner"></div>
             <div className="loader-text">Loading...</div>
         </div>
     );
