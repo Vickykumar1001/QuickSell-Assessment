083d7b5a45aa41752a67fc9288bb42a8fb5b90fb
vickykumar1001 <vicky9060993097@gmail.com>
2024-11-16 23:01:54 +0530

    Added Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => (
+    <header className="header-container">
+        <p>Display</p>
+    </header>
+);
+
+export default Header;
