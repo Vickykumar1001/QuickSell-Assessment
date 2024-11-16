4b7255ac331a525f4366f28f2e2f012775a26a97
vickykumar1001 <vicky9060993097@gmail.com>
2024-11-17 03:39:24 +0530

    Updated UserIcon - added availability indicator and random backgroun color

--- src/components/UserIcon/index.jsx
+++ src/components/UserIcon/index.jsx
@@ -1,18 +1,17 @@
 import React, { useMemo } from 'react';
 import './userIcon.css';
 
-function UserIcon({ name }) {
-    const initials = useMemo(() => {
-        return name
-            .split(" ")
-            .map((word) => word[0].toUpperCase())
-            .join("");
+function UserIcon({ name, available }) {
+    const initials = useMemo(() => name.split(" ").map((w) => w[0].toUpperCase()).join(""), [name]);
+    const backgroundColor = useMemo(() => {
+        const hash = name.split("").reduce((acc, char) => acc + char.charCodeAt(0), 0);
+        return ["#FF5733", "#33C1FF", "#FFC133", "#8D33FF", "#33FF57"][hash % 5];
     }, [name]);
 
     return (
-        <div className="user-icon">
+        <div className="user-icon" style={{ backgroundColor }}>
             <span className="user-initials">{initials}</span>
-            <span className="status-indicator"></span>
+            <span className={`status-indicator ${available && "available"}`}></span>
         </div>
     );
 }
